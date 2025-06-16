2ebc8b55fe254c4aff42484853c75a0ff51a193b
William de Oliveira de Liz <william_liz@estudante.sesisenai.org.br>
2025-06-16 19:17:25 -0300

    Criação do projeto de epis com Springboot

---
+++ src/main/java/com/exemplo/epi/EpiController.java
@@ -0,0 +1,25 @@
+package com.exemplo.epi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@Controller
+public class EpiController {
+
+    @Autowired
+    private EpiRepository epiRepository;
+
+    @PostMapping("/epis")
+    public String salvar(@RequestParam String nome, @RequestParam String validade) {
+        epiRepository.salvar(new Epi(nome, validade));
+        return "redirect:/epis";
+    }
+
+    @GetMapping("/epis")
+    @ResponseBody
+    public List<Epi> listar() {
+        return epiRepository.buscarTodos();
+    }
+}
