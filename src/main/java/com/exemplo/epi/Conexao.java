5a099f24fea2a7cb49eee90eb0728e603c6affd8
William de Oliveira de Liz <william_liz@estudante.sesisenai.org.br>
2025-06-24 19:08:13 -0300

    bugfix

--- src/main/java/com/exemplo/epi/Conexao.java
+++ src/main/java/com/exemplo/epi/Conexao.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String URL = "jdbc:mysql://localhost:3306/controle_epis";
+    private static final String URL = "jdbc:mysql://localhost:3306/episdb";
     private static final String USUARIO = "root";
     private static final String SENHA = "";
 
